bb27bd43e87509c4380da37240305a8815ff3bed
EC2 Default User <ec2-user@ip-172-31-6-17.ec2.internal>
2022-01-28 05:47:03 +0000

    [Update]bookのindexページを作成,あとリンク先の設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,31 @@
 class BooksController < ApplicationController
-  def index
-  end
-
   def show
   end
 
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
   def new
-    @list = List.new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
